333ccde82f857885fb943214180520fe37de7dde
Hungzazed <119870697+Hungzazed@users.noreply.github.com>
2025-09-05 18:58:26 +0700

    update upload file

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const { isValidUrl } = require("./utils/checkValidUrl");
 const { encodeBase62 } = require("./utils/base62");
 const { google } = require("googleapis");
 const multer = require("multer");
-const { loadOrSetCredentials } = require("./utils/loadToken");
 const fs = require("fs");
 const path = require("path");
 
@@ -29,12 +28,38 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_REDIRECT_URI
 );
 
+async function loadOrSetCredentials() {
+  const savedToken = process.env.GOOGLE_TOKEN;
+  if (savedToken) {
+    const credentials = JSON.parse(savedToken)
+    oauth2Client.setCredentials(credentials);
+    return;
+  }
+  const tokenPath = path.join(__dirname, "token.json");
+  try {
+    const content = await fs.readFile(tokenPath);
+    const credentials = JSON.parse(content);
+    oauth2Client.setCredentials(credentials);
+  } catch (err) {
+    return getNewToken(oauth2Client);
+  }
+}
+
+async function getNewToken(oauth2Client) {
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: ["https://www.googleapis.com/auth/drive.file"],
+    prompt: "consent"
+  });
+  console.log("Authorize this app by visiting this url:", authUrl);
+}
+
+loadOrSetCredentials().catch(console.error);
+
 const drive = google.drive({
   version: "v3",
   auth: oauth2Client,
 });
-
-loadOrSetCredentials().catch(console.error);
 
 // Middleware kiểm tra lỗi upload
 app.use((err, req, res, next) => {
--- utils/loadToken.js
+++ utils/loadToken.js
@@ -14,29 +14,7 @@ const drive = google.drive({
   auth: oauth2Client,
 });
 
-async function loadOrSetCredentials() {
-  const savedToken = process.env.GOOGLE_TOKEN;
-  if (savedToken) {
-    oauth2Client.setCredentials(JSON.parse(savedToken));
-    return;
-  }
-  const tokenPath = path.join(__dirname, "token.json");
-  try {
-    const content = await fs.readFile(tokenPath);
-    const credentials = JSON.parse(content);
-    oauth2Client.setCredentials(credentials);
-  } catch (err) {
-    return getNewToken(oauth2Client);
-  }
-}
 
-async function getNewToken(oauth2Client) {
-  const authUrl = oauth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: ["https://www.googleapis.com/auth/drive.file"],
-  });
-  console.log("Authorize this app by visiting this url:", authUrl);
-}
 
 
 module.exports = { loadOrSetCredentials, getNewToken, drive };
